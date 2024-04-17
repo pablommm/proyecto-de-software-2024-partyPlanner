4e23373bea9f6bedbd3a54304c51559ebd9cf81a
pablommm <murgiapablo@gmail.com>
2024-04-17 17:01:06 -0300

    se crea usuario de ejemplo

--- src/main/kotlin/grupo_4/partyplannerbackend/bootstrap/PartyPlannerBootstrap.kt
+++ src/main/kotlin/grupo_4/partyplannerbackend/bootstrap/PartyPlannerBootstrap.kt
@@ -1,11 +1,26 @@
 package grupo4.partyplannerbackend.bootstrap
 
+import grupo_4.partyplannerbackend.Repositorio.RepoUser
+import grupo_4.partyplannerbackend.domain.Usuario
 import org.springframework.beans.factory.InitializingBean
+import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.stereotype.Service
 
 @Service
 class PartyPlannerBootstrap : InitializingBean {
     override fun afterPropertiesSet() {
-        TODO("Not yet implemented")
+        crearUser()
+    }
+
+    @Autowired
+    lateinit var repoUser: RepoUser
+
+    val usuarioPrueba = Usuario(
+        nombreYApellido = "juanperez1",
+        username = "sarasa",
+        contraseña = "1234"
+    )
+    fun crearUser(){
+        repoUser.create(usuarioPrueba)
     }
 }
